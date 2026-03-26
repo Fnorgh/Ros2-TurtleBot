34e761256d1b6ce029f7e6d1d77ae4bafd794d02
jacers <jacerausch@gmail.com>
2026-03-26 13:13:22 -0500

    XML files added for launch

--- ros2_ws/src/reactive_robot/setup.py
+++ ros2_ws/src/reactive_robot/setup.py
@@ -10,6 +10,10 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', [
+            'launch/reactive_robot.launch.py',
+            'launch/project2.launch.xml',
+        ]),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
