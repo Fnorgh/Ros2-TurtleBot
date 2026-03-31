09947e6d25d401600eee9091194fba1b2283b1e6
Fnorgh <102239083+Fnorgh@users.noreply.github.com>
2026-03-31 12:20:49 -0500

    asdf

--- ros2_ws/src/reactive_robot/launch/reactive_robot.launch.py
+++ ros2_ws/src/reactive_robot/launch/reactive_robot.launch.py
@@ -1,7 +1,13 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.actions import ExecuteProcess
 
 def generate_launch_description():
+
+    kill_joystick = ExecuteProcess(
+        cmd=['bash', '-lc', 'pkill -f teleop_twist_joy 2>/dev/null; true'],
+        output='screen'
+    )
 
     reactive_node = Node(
         package='reactive_robot',
@@ -11,5 +17,6 @@ def generate_launch_description():
     )
 
     return LaunchDescription([
-        reactive_node
+        kill_joystick,
+        reactive_node,
     ])
